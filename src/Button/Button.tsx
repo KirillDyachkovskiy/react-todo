f5e5febaa29cd370c4e5010d6682d44d9b81ba14
Kirill <kirilldyachkovskiy@gmail.com>
2022-04-15 16:36:14 +0300

    add button compoenent

---
+++ src/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './button.module.css';
+
+interface IButton {
+  children: string;
+}
+
+export default function Button({children}: IButton) {
+  return <button className={s.button}>{children}</button>
+  ;
+}
